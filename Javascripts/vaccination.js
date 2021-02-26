0e99a4202078e96532786e4abaa720416f0c5a32
Rich Tyler <rich.tyler.2018@hotmail.com>
2021-02-26 17:28:45 +0000

    Feb 26 Update

--- Javascripts/vaccination.js
+++ Javascripts/vaccination.js
@@ -45,3 +45,123 @@ function loadTable_ltla_vaccine(vaccine_at_a_glance) {
 
   tableBody.innerHTML = dataHTML;
 }
+
+// Maps
+
+var msoa_covid_vaccines_raw = [
+  "Up to 1,999",
+  "2,000-2,999",
+  "3,000-3,999",
+  "4,000-4,999",
+  "5,000-5,999",
+  "6,000-6,9999",
+  "7,000+",
+];
+
+var msoa_covid_vaccines_colours = [
+  "#ffffcc",
+  "#c7e9b4",
+  "#7fcdbb",
+  "#41b6c4",
+  "#1d91c0",
+  "#225ea8",
+  "#0c2c84",
+];
+
+var msoa_covid_vaccines_colour_func = d3
+  .scaleOrdinal()
+  .domain(msoa_covid_vaccines_raw)
+  .range(msoa_covid_vaccines_colours);
+
+// Add AJAX request for data
+var msoa_vaccine_total = $.ajax({
+  url: "./Outputs/msoa_covid_vaccine_latest.geojson",
+  dataType: "json",
+  success: console.log("MSOA boundary data successfully loaded."),
+  error: function (xhr) {
+    alert(xhr.statusText);
+  },
+});
+
+var tileUrl = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
+var attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a><br> Contains Ordnance Survey data © Crown copyright and database right 2020.<br>Zoom in/out using your mouse wheel or the plus (+) and minus (-) buttons. Click on an area to find out more';
+
+function vaccine_msoa_colour(d) {
+  return d === msoa_covid_vaccines_raw[0]
+    ? msoa_covid_vaccines_colours[0]
+    : d === msoa_covid_vaccines_raw[1]
+    ? msoa_covid_vaccines_colours[1]
+    : d === msoa_covid_vaccines_raw[2]
+    ? msoa_covid_vaccines_colours[2]
+    : d === msoa_covid_vaccines_raw[3]
+    ? msoa_covid_vaccines_colours[3]
+    : d === msoa_covid_vaccines_raw[4]
+    ? msoa_covid_vaccines_colours[4]
+    : d === msoa_covid_vaccines_raw[5]
+    ? msoa_covid_vaccines_colours[5]
+    : d === msoa_covid_vaccines_raw[6]
+    ? msoa_covid_vaccines_colours[6]
+    : "#feebe2";
+}
+
+function style_msoa_vaccine_total(feature) {
+  return {
+    fillColor: vaccine_msoa_colour(feature.properties.Total_banded),
+    weight: 1,
+    opacity: 1,
+    color: "#c9c9c9",
+    // dashArray: "3",
+    fillOpacity: 1,
+  };
+}
+
+// msoa_map_vaccine;
+
+$.when(msoa_vaccine_total).done(function () {
+  var msoa_map_vaccine_leaf = L.map("msoa_map_vaccine");
+
+  var basemap_msoa_vaccine = L.tileLayer(tileUrl, {
+    attribution,
+    minZoom: 8,
+  }).addTo(msoa_map_vaccine_leaf);
+
+  var msoa_vaccine_total_hcl = L.geoJSON(msoa_vaccine_total.responseJSON, {
+    style: style_msoa_vaccine_total,
+  })
+    .addTo(msoa_map_vaccine_leaf)
+    .bindPopup(function (layer) {
+      return (
+        "<p><b>" +
+        layer.feature.properties.msoa11nm +
+        " (" +
+        layer.feature.properties.LTLA_name +
+        ")</b></p><p>A total of <b>" +
+        d3.format(",.0f")(layer.feature.properties.Total_where_age_known) +
+        "</b> people have received at least one dose of a COVID-19 vaccine.</p><p>Data correct as at " +
+        vaccine_update_date
+      );
+    });
+
+  msoa_map_vaccine_leaf.fitBounds(msoa_vaccine_total_hcl.getBounds());
+});
+
+function key_msoa_vaccines() {
+  msoa_covid_vaccines_raw.forEach(function (item, index) {
+    var list = document.createElement("li");
+    list.innerHTML = item;
+    list.className = "key_list";
+    list.style.borderColor = msoa_covid_vaccines_colour_func(index);
+    var tt = document.createElement("div");
+    tt.className = "side_tt";
+    tt.style.borderColor = msoa_covid_vaccines_colour_func(index);
+    var tt_h3_1 = document.createElement("h3");
+    tt_h3_1.innerHTML = item;
+
+    tt.appendChild(tt_h3_1);
+    var div = document.getElementById("msoa_vaccine_key");
+    div.appendChild(list);
+  });
+}
+
+key_msoa_vaccines();
--- R scripts/C19_vaccination_data_reporting.R
+++ R scripts/C19_vaccination_data_reporting.R
@@ -21,9 +21,11 @@ vaccine_meta <- read_excel(paste0(github_repo_dir,'/Source files/nhs_e_vaccines.
                                       n_max = 7,
                            col_names = c('item', 'description'))
 
-as.character(vaccine_meta %>% 
+vaccine_published_date <- as.character(vaccine_meta %>% 
   filter(item == 'Published:') %>% 
-  select(description)) %>% 
+  select(description)) 
+
+vaccine_published_date %>% 
   toJSON() %>% 
   write_lines(paste0(output_directory_x, '/vaccine_update_date.json'))
 
@@ -32,6 +34,11 @@ as.character(vaccine_meta %>%
                select(description)) %>% 
   toJSON() %>% 
   write_lines(paste0(output_directory_x, '/vaccine_administered_date.json'))
+
+mye_ages_70_plus <- mye_ages %>% 
+  filter(Age %in% c('70-74 years', '75-79 years', '80+ years')) %>% 
+  group_by(Name, Code, Type) %>% 
+  summarise(Population_70plus = sum(Population))
 
 # LTLA vaccine data ####
 
@@ -47,23 +54,45 @@ vaccine_df_ltla <- read_excel(paste0(github_repo_dir,'/Source files/nhs_e_vaccin
   mutate(Rate_age_known_ucl = pois.exact(Total_where_age_known, Population)[[5]]*100000) %>% 
   rename(Name = LTLA_name)
 
+vaccine_df_ltla_70_plus <- vaccine_df_ltla %>% 
+  pivot_longer(cols =  c(Under_70, `70_74`, `75_79`, `80_and_over`),
+               names_to = 'Ages',
+               values_to = 'Individuals') %>% 
+  mutate(Age_banding = ifelse(Ages %in% c('70_74', '75_79', '80_and_over'), '70 and over', Ages)) %>% 
+  group_by(LTLA_code, Age_banding) %>% 
+  summarise(Individuals = sum(Individuals)) %>% 
+  ungroup() %>% 
+  filter(Age_banding == '70 and over') %>% 
+  left_join(mye_ages_70_plus, by = c('LTLA_code' = 'Code')) %>% 
+  mutate(Rate_70_plus_per_100000 = pois.exact(Individuals, Population_70plus)[[3]]*100000) %>% 
+  mutate(Rate_70_plus_known_lcl = pois.exact(Individuals, Population_70plus)[[4]]*100000) %>% 
+  mutate(Rate_70_plus_known_ucl = pois.exact(Individuals, Population_70plus)[[5]]*100000) %>% 
+  rename(Individuals_70_plus = Individuals) %>% 
+  mutate(Proportion_70_plus = Individuals_70_plus/ Population_70plus) %>% 
+  select(LTLA_code, Individuals_70_plus, Rate_70_plus_per_100000, Proportion_70_plus)
 
+vaccine_df_ltla <- vaccine_df_ltla %>% 
+  left_join(vaccine_df_ltla_70_plus, by = 'LTLA_code')
+  
 vaccine_df_wsx <-vaccine_df_ltla %>% 
   filter(Name %in% c('Adur', 'Arun', 'Chichester', 'Crawley', 'Horsham', 'Mid Sussex', 'Worthing')) %>%
-  select(Total_where_age_known) %>% 
-  summarise(Total_where_age_known = sum(Total_where_age_known)) %>% 
+  select(Total_where_age_known, Individuals_70_plus) %>% 
+  summarise(Total_where_age_known = sum(Total_where_age_known),
+            Individuals_70_plus = sum(Individuals_70_plus)) %>% 
   mutate(Name = 'West Sussex',
          Code = 'E10000032') %>% 
   left_join(mye_total, by = 'Code') %>% 
   mutate(Rate_age_known_per_100000 = pois.exact(Total_where_age_known, Population)[[3]]*100000) %>% 
   mutate(Rate_age_known_lcl = pois.exact(Total_where_age_known, Population)[[4]]*100000) %>% 
-  mutate(Rate_age_known_ucl = pois.exact(Total_where_age_known, Population)[[5]]*100000)
-  
+  mutate(Rate_age_known_ucl = pois.exact(Total_where_age_known, Population)[[5]]*100000) %>% 
+  left_join(mye_ages_70_plus, by = 'Code') %>% 
+  mutate(Rate_70_plus_per_100000 = pois.exact(Individuals_70_plus, Population_70plus)[[3]]*100000) %>% 
+  mutate(Proportion_70_plus = Individuals_70_plus/ Population_70plus)
 
 vaccine_df_ltla %>% 
   filter(Name %in% c('Adur', 'Arun', 'Chichester', 'Crawley', 'Horsham', 'Mid Sussex', 'Worthing')) %>% 
   bind_rows(vaccine_df_wsx) %>% 
-  select(Name, Total_where_age_known, Rate_age_known_per_100000) %>% 
+  select(Name, Total_where_age_known, Rate_age_known_per_100000, Individuals_70_plus, Rate_70_plus_per_100000, Proportion_70_plus) %>% 
   toJSON() %>% 
   write_lines(paste0(output_directory_x, '/vaccine_at_a_glance.json'))
 
@@ -73,6 +102,11 @@ lad_boundary <- geojson_read('https://opendata.arcgis.com/datasets/69cd46d7d2664
   rename(LAD19CD = id) %>%
   filter(substr(LAD19CD, 1,1 ) == 'E') %>% 
   left_join(vaccine_df_ltla, by = c('LAD19CD' = 'LTLA_code'))
+
+# This will read in the boundaries (in a geojson format) from Open Geography Portal
+County_boundary <- geojson_read("https://opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.geojson",  what = "sp") %>% 
+  filter(ctyua19nm %in% c('West Sussex', 'East Sussex', 'Brighton and Hove')) %>%
+  spTransform(CRS("+init=epsg:4326"))
 
 ggplot() +
   coord_fixed(1.5) +
@@ -87,7 +121,6 @@ ggplot() +
                alpha = 1,
                show.legend = TRUE) +
   scale_fill_viridis_c(option = "plasma", 
-                       trans = "sqrt",
                        label = comma,
                        name = 'Rate (where age known)\nper 100,000 population\n(all ages)') +
   geom_polygon(data = County_boundary,
@@ -97,8 +130,34 @@ ggplot() +
                colour = '#000000',
                fill = NA,
                size = .2) +
-  labs(title = paste0('Cumulative rate of individuals receiving at least one dose of a COVID-19 vaccine per 100,000 population (all ages);\n; Includes inviduals where age was recorded; Lower Tier Local and Unitary Authorities'),
-       subtitle = paste0('Data as at ', format(vaccine_published_date, '%d %B %Y')))  +
+  labs(title = paste0('Cumulative rate of individuals receiving at least one dose of a COVID-19 vaccine per 100,000 population (all ages);\n Includes inviduals where age was recorded; Lower Tier Local and Unitary Authorities'),
+       subtitle = paste0('Data as at ', vaccine_published_date))  +
+  theme(legend.position = 'right')
+
+ggplot() +
+  coord_fixed(1.5) +
+  map_theme() +
+  geom_polygon(data = lad_boundary,
+               aes(x=long,
+                   y=lat,
+                   group = group,
+                   fill = Individuals_70_plus),
+               color="#ffffff",
+               size = .1,
+               alpha = 1,
+               show.legend = TRUE) +
+  scale_fill_viridis_c(option = "plasma", 
+                       label = comma,
+                       name = 'Rate (where age known)\nper 100,000 population\n(aged 70 and over)') +
+  geom_polygon(data = County_boundary,
+               aes(x = long,
+                   y = lat,
+                   group = group),
+               colour = '#000000',
+               fill = NA,
+               size = .2) +
+  labs(title = paste0('Cumulative rate of individuals aged 70+ receiving at least one dose of a COVID-19 vaccine per 100,000 population (aged 70+);\n; Includes inviduals where age was recorded; Lower Tier Local and Unitary Authorities'),
+       subtitle = paste0('Data as at ', vaccine_published_date))  +
   guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
   theme(legend.position = 'bottom')
 
@@ -113,43 +172,46 @@ vaccine_df_msoa <- read_excel(paste0(github_repo_dir,'/Source files/nhs_e_vaccin
                               col_names = c('Region_code', 'Region_name', 'LTLA_code', 'LTLA_name', 'msoa11cd', 'msoa11nm', 'Under_70', '70_74', '75_79', '80_and_over')) %>% 
   filter(!is.na(Region_name)) %>% 
   mutate(Total_where_age_known = Under_70 + `70_74` + `75_79` + `80_and_over`) %>% 
-  left_join(msoa_names[c('msoa11cd', 'msoa11hclnm')], by = 'msoa11cd')
-
-# This will read in the boundaries (in a geojson format) from Open Geography Portal
-County_boundary <- geojson_read("https://opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.geojson",  what = "sp") %>% 
-  filter(ctyua19nm %in% c('West Sussex', 'East Sussex', 'Brighton and Hove')) %>%
-  spTransform(CRS("+init=epsg:4326"))
-
+  left_join(msoa_names[c('msoa11cd', 'msoa11hclnm')], by = 'msoa11cd') %>% 
+  filter(LTLA_name %in% c('Brighton and Hove','Eastbourne', 'Hastings', 'Lewes','Rother','Wealden','Adur', 'Arun', 'Chichester', 'Crawley','Horsham','Mid Sussex', 'Worthing')) %>% 
+  mutate(Total_banded = factor(ifelse(Total_where_age_known < 2000, 'Up to 1,999', ifelse(Total_where_age_known < 3000, '2,000-2,999', ifelse(Total_where_age_known < 4000, '3,000-3,999', ifelse(Total_where_age_known < 5000, '4,000-4,999',  ifelse(Total_where_age_known < 6000, '5,000-5,999',  ifelse(Total_where_age_known < 7000, '6,000-6,999', '7,000+')))))), levels = c('Up to 1,999', '2,000-2,999', '3,000-3,999', '4,000-4,999', '5,000-5,999', '6,000-6,999', '7,000+'))) %>% 
+  arrange(msoa11cd)
 
 MSOA_boundary <- geojson_read('https://github.com/psychty/wsx_covid_datapack_public/raw/master/Source%20files/failsafe_msoa_boundary.geojson', what = 'sp') %>% 
-  filter(MSOA11CD %in% vaccine_df_msoa$msoa11cd)
+  filter(MSOA11CD %in% vaccine_df_msoa$msoa11cd) %>% 
+  arrange(MSOA11CD)
 
+df <- data.frame(ID = character())
 
-constituency_boundary <- geojson_read('https://opendata.arcgis.com/datasets/102b0e202e934fc7a13e7906e77b7a1e_0.geojson', what = 'sp')
-%>% 
-  
-view(constituency_boundary)
+# Get the IDs of spatial polygon
+for (i in MSOA_boundary@polygons ) { df <- rbind(df, data.frame(ID = i@ID, stringsAsFactors = FALSE))  }
+# and set rowname = ID
+row.names(vaccine_df_msoa) <- df$ID
+# Then use df as the second argument to the spatial dataframe conversion function:
+MSOA_boundary <- SpatialPolygonsDataFrame(MSOA_boundary, vaccine_df_msoa)  
+
+geojson_write(ms_simplify(geojson_json(MSOA_boundary), keep = 0.2), file = paste0(output_directory_x, '/msoa_covid_vaccine_latest.geojson'))
 
 MSOA_boundary_gg <- MSOA_boundary %>%
-  patched_fortify(region = "MSOA11CD") %>% 
-  rename(msoa11cd = id)
-
-MSOA_cumulative_map <- MSOA_boundary_gg %>% 
-  left_join(vaccine_msoa_cumulative_doses, by = 'msoa11cd')
+  fortify(region = "MSOA11CD") %>%
+  rename(msoa11cd = id) %>%
+  left_join(vaccine_df_msoa, by = 'msoa11cd')
 
 ggplot() +
   coord_fixed(1.5) +
   map_theme() +
-  geom_polygon(data = MSOA_cumulative_map,
+  geom_polygon(data = MSOA_boundary_gg,
                aes(x=long,
                    y=lat,
                    group = group,
-                   fill = Dose_1_cumulative),
+                   fill = Total_banded),
                color="#5d5d5d",
                size = .1,
                alpha = 1,
                show.legend = TRUE) +
-  scale_fill_viridis_c(option = "plasma", trans = "sqrt") +
+  scale_fill_manual(name = 'Number of individuals\nreceiving at least\none dose',
+                    values = c('#ffffcc','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#0c2c84'),
+                    drop = FALSE) +
   geom_polygon(data = County_boundary,
                aes(x = long,
                    y = lat,
@@ -157,7 +219,8 @@ ggplot() +
                colour = '#000000',
                fill = NA,
                size = .2) +
-  labs(title = paste0('Cumulative number of first dose Covid-19 vaccinations administered;\nSussex MSOAs;'),
-       subtitle =paste0('Data as at ', format(date_vaccine_latest, '%d %B %Y'), ' for vaccines given up to ', format(date_vaccine_latest - 2, '%d %B %Y')))  +
-  guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
-  theme(legend.position = 'bottom')
+  labs(title = paste0('Cumulative number of individuals receiving at least Covid-19 vaccination dose;\nSussex MSOAs;'),
+       subtitle =paste0('Data as at ', vaccine_published_date))  +
+  # guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
+  theme(legend.position = 'right')
+
